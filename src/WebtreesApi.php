08c04a7d3e392a1b11433fbae4562c877aa0d461
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2026-08-04 22:06:29 +0200

    Increased version to 1.2.2

--- src/WebtreesApi.php
+++ src/WebtreesApi.php
@@ -139,7 +139,7 @@ class WebtreesApi extends AbstractModule implements
     private Filesystem $data_filesystem;
 
 	// Custom module version
-	public const CUSTOM_VERSION = '1.2.1';
+	public const CUSTOM_VERSION = '1.2.2';
 
 	// Routes
     public const string ROUTE_MCP                 = '/mcp';
